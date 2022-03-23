055d95d2a78bfc83c530c9e985635d014f66f13e
Gonzalo <gonzalopiombi@gmail.com>
2022-03-23 16:55:23 -0300

    When user clicks an option from the dropdown menu it checks if the character is there and displays a message with the result.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,58 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import Header from './components/Header';
 import Dropdown from './components/Dropdown';
 import gameImage from './images/wally.jpg';
 import './App.css';
+import { getFirestore, collection, getDocs } from 'firebase/firestore';
 
 function App() {
   const [dropdown, setDropdown] = useState(false);
   const [x, setX] = useState(null);
   const [y, setY] = useState(null);
+  const [width, setWidth] = useState(null);
+  const [height, setHeight] = useState(null);
+
+  useEffect(() => {
+    console.log(x, y, width, height);
+    console.log(x / width, y / height);
+  }, [x, y, width, height]);
 
   const handleClick = (e) => {
-    console.log(e);
     setDropdown(!dropdown);
     setX(e.pageX);
     setY(e.pageY);
-    const width = e.target.offsetWidth;
-    const height = e.target.offsetHeight;
+    setWidth(e.target.offsetWidth);
+    setHeight(e.target.offsetHeight);
+  };
 
-    console.log(x, y, width, height);
-    console.log(x / width, y / height);
+  const handleSelection = (e) => {
+    //Check if coordinates are within the character boundaries.
+    const db = getFirestore();
+    const colRef = collection(db, 'characters');
+
+    getDocs(colRef)
+      .then((snapshot) => {
+        let characters = [];
+        snapshot.docs.forEach((doc) => {
+          characters.push({ ...doc.data(), id: doc.id });
+        });
+        const char = characters.find(
+          (char) => char.name === e.target.textContent
+        );
+        if (
+          x / width > char.minX &&
+          x / width < char.maxX &&
+          y / height > char.minY &&
+          y / height < char.maxY
+        ) {
+          alert(`You found ${char.name}!`);
+        } else {
+          alert('Keep looking!');
+        }
+      })
+      .catch((error) => console.log(error));
+
+    setDropdown(!dropdown);
   };
 
   return (
@@ -29,8 +63,9 @@ function App() {
         alt="Where's Waldo"
         className="game-image"
         onClick={handleClick}
+        draggable="false"
       />
-      {dropdown ? <Dropdown x={x} y={y} /> : null}
+      {dropdown ? <Dropdown x={x} y={y} onSelection={handleSelection} /> : null}
     </div>
   );
 }
