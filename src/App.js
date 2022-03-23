9fe2596b58d5acb00fd1d6ab43e04763149a7402
Gonzalo <gonzalopiombi@gmail.com>
2022-03-23 15:07:54 -0300

    Render the header, game image and render conditionally the context menu.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,38 @@
+import { useState } from 'react';
+import Header from './components/Header';
+import Dropdown from './components/Dropdown';
+import gameImage from './images/wally.jpg';
+import './App.css';
+
 function App() {
-  return <div className="App"></div>;
+  const [dropdown, setDropdown] = useState(false);
+  const [x, setX] = useState(null);
+  const [y, setY] = useState(null);
+
+  const handleClick = (e) => {
+    console.log(e);
+    setDropdown(!dropdown);
+    setX(e.pageX);
+    setY(e.pageY);
+    const width = e.target.offsetWidth;
+    const height = e.target.offsetHeight;
+
+    console.log(x, y, width, height);
+    console.log(x / width, y / height);
+  };
+
+  return (
+    <div className="App">
+      <Header />
+      <img
+        src={gameImage}
+        alt="Where's Waldo"
+        className="game-image"
+        onClick={handleClick}
+      />
+      {dropdown ? <Dropdown x={x} y={y} /> : null}
+    </div>
+  );
 }
 
 export default App;
