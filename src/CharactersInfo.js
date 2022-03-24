3ca7cdf6bc4177a15b0d0c9904aae421d1f368c7
Gonzalo <gonzalopiombi@gmail.com>
2022-03-24 15:40:19 -0300

    Array of objects with the character info.

---
+++ src/CharactersInfo.js
@@ -0,0 +1,23 @@
+import waldo from './images/waldo.jpg';
+import odlaw from './images/odlaw.jpg';
+import wizard from './images/wizard.jpg';
+
+const charactersInfo = [
+  {
+    name: 'Waldo',
+    found: false,
+    image: waldo,
+  },
+  {
+    name: 'Odlaw',
+    found: false,
+    image: odlaw,
+  },
+  {
+    name: 'Whitebeard',
+    found: false,
+    image: wizard,
+  },
+];
+
+export default charactersInfo;
