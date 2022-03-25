f8b5535213558a4ca04511025727d39b882daa11
Gonzalo <gonzalopiombi@gmail.com>
2022-03-25 15:22:19 -0300

    When user scrolls the opacity of the header changes and when is back on top goes back to the original value.

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,32 @@
+import { useEffect, useState } from 'react';
+
 const Header = (props) => {
+  const [opacity, setOpacity] = useState(1);
+
+  useEffect(() => {
+    const onScroll = () =>
+      window.scrollY === 0 ? setOpacity(1) : setOpacity(0.8);
+    window.addEventListener('scroll', onScroll);
+    return () => window.removeEventListener('scroll', onScroll);
+  }, []);
+
   return (
-    <header>
+    <header style={{ opacity: opacity }}>
       <h1>Where's Waldo</h1>
-      <button onClick={props.onBtnClick} autoFocus>
+      <div>
+        {props.characters.map((character) => (
+          <img
+            style={{ opacity: character.found ? 0.2 : 1 }}
+            className="headshot"
+            src={character.image}
+            alt={character.name}
+            key={character.name}
+          />
+        ))}
+      </div>
+      <p>Time</p>
+      <button>Leaderboard</button>
+      <button className="help-button" onClick={props.onBtnClick}>
         ?
       </button>
     </header>
