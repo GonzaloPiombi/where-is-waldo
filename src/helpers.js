958abc85406f1cdfc90fa61aa3608700d9f8e910
Gonzalo <gonzalopiombi@gmail.com>
2022-03-28 16:15:37 -0300

    Add formatTime function to helpers file.

---
+++ src/helpers.js
@@ -0,0 +1,9 @@
+const formatTime = (time) => {
+  const seconds = `0${Math.round(time % 60)}`.slice(-2);
+  const mins = `${Math.floor(time / 60)}`;
+  const minutes = `0${Math.floor(mins % 60)}`.slice(-2);
+  const hours = `0${Math.floor(time / 3600)}`.slice(-2);
+  return `${hours}:${minutes}:${seconds}`;
+};
+
+export { formatTime };
