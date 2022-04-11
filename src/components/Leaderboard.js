51726fc2b6cd0619b621860702a7ec0f983ce020
Gonzalo <gonzalopiombi@gmail.com>
2022-04-11 16:13:47 -0300

    Create logic that populates the leaderboard.

--- src/components/Leaderboard.js
+++ src/components/Leaderboard.js
@@ -1,14 +1,58 @@
+import { useEffect, useState } from 'react';
+import { query, orderBy, getDocs } from '@firebase/firestore';
+import { formatTime } from '../helpers';
+
 const Leaderboard = (props) => {
+  const [score, setScore] = useState([]);
+
+  useEffect(() => {
+    console.log('Component did mount');
+    // const colRef = collection(props.db, 'leaderboard');
+    const q = query(props.colRef, orderBy('totalTime'));
+
+    const showLeaderboard = async () => {
+      return getDocs(q).then((snapshot) => {
+        let score = [];
+        snapshot.docs.forEach((doc) => {
+          const data = doc.data();
+          console.log(data);
+          if (data.totalTime === null) {
+            return;
+          } else {
+            score.push({ ...doc.data(), id: doc.id });
+          }
+        });
+        //Format the time as it is displayed as seconds currently.
+        score.forEach((entry) => {
+          entry.totalTime = formatTime(entry.totalTime);
+        });
+        return score;
+      });
+    };
+
+    showLeaderboard().then((res) => {
+      console.log(res);
+      setScore(res);
+    });
+  }, []);
+
   return (
-    <div className="modal" style={{ flexDirection: 'column' }}>
-      {props.score.map((entry) => {
-        return (
-          <div key={entry.id} className="leaderboard-entry">
-            <p>{entry.name}</p>
-            <p>{entry.totalTime}</p>
-          </div>
-        );
-      })}
+    <div className="modal">
+      <div className="leaderboard-container">
+        <div className="leaderboard-entry categories">
+          <p>Name</p>
+          <p>Time</p>
+        </div>
+        {score.map((entry) => {
+          return (
+            <div key={entry.id} className="leaderboard-entry">
+              <p>{entry.name}</p>
+              <p>{entry.totalTime}</p>
+            </div>
+          );
+        })}
+        <button onClick={props.closeLeaderboard}>Close</button>
+      </div>
     </div>
   );
 };
